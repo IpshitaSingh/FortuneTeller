f785274dc3f3d26c54c3bf4575ba3ecee6b7c069
IpshitaSingh <ipshita0501@gmail.com>
2020-04-17 01:53:56 +0530

    first commit

---
+++ fortunemain.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template('mainpage.html')
+
+
+@app.route("/show")
+def show():
+    z = ['Focus on your health and well-being.', 'Get ready for a life changing event!',
+          'Sooner or later, you need to take a break', 'You will make a new friend',
+          'Treat others with more compassion.']
+    a = random.choice(z)
+    a = a
+    return render_template('show.html', a=a)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
